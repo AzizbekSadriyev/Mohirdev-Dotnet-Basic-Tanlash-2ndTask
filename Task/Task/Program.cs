448d1f877e799d27580ea695e66109a8e196d386
Azizbek Sadriyev <azizbeksadriyev@gmail.com>
2024-03-03 20:25:44 +0500

    Modified Program.cs file

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Task
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Task list:");
+            Console.WriteLine("1. Time calculator");
+            Console.WriteLine("2. Determination of age category");
+            Console.WriteLine("3. Calculate the average score");
+            Console.WriteLine("4. Game of \"Find the number\"");
+            Console.Write("To select one of the tasks, enter its number: ");
+            short taskNumber = Convert.ToInt16(Console.ReadLine());
+            switch (taskNumber)
+            {
+                case 1:
+                    FirstTask.Task();
+                    break;
+                case 2:
+                    SecondTask.Task();
+                    break;
+                case 3:
+                    ThirdTask.Task();
+                    break;
+                case 4:
+                    FourthTask.Task();
+                    break;
+                default:
+                    Console.WriteLine("Task Not Found!");
+                    break;
+            }
+        }
+    }
+}
